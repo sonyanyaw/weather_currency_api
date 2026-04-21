8d578750aa3b598004b29c4ce5a34ff83cf17111
Sonya <preminina.sofya@gmail.com>
2026-04-21 14:01:45 +0300

    feat: add centralized configuration using Pydantic Settings

---
+++ src/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    weather_api_key: str
+    currencyrate_api_key: str
+
+    cache_ttl_weather: int = 1800
+    cache_ttl_currency: int = 3600
+    
+    redis_url: str
+
+    weather_base_url: str = "https://www.meteosource.com/api/v1/free/point"
+    currency_base_url: str = "https://v6.exchangerate-api.com/v6"
+
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
